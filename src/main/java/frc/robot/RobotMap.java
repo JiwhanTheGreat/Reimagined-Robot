569e2c7f8e03d1ec6909d58c04b9a2e1a71dee5a
Jiwhan Kim <jiwhan.kim2002@gmail.com>
2019-07-30 20:51:56 -0400

    added elevator constants to robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,19 +1,36 @@
 package frc.robot;
 
+/**
+ * Stores constants for use throughout the project, including but not limited to motor controller
+ * constants and controller mappings
+ * @author jk
+ */
+
 public class RobotMap{
     public static class Drivetrain{
-        public static final int LeftPrimary = 23;
-        public static final int LeftSecondary = 24;
-        public static final int LeftTertiary = 25;
+        public static final int LEFT_PRIMARY = 23; //untested
+        public static final int LEFT_SECONDARY = 24; //untested
+        public static final int LEFT_TERTIARY = 25; //untested
 
-        public static final int RightPrimary = 10;
-        public static final int RightSecondary = 11;
-        public static final int RightTertiary = 12;
+        public static final int RIGHT_PRIMARY = 10; //untested
+        public static final int RIGHT_SECONDARY = 11; //untested
+        public static final int RIGHT_TERTIARY = 12; //untested
 
     }
 
     public static class ClimberElevator{
-        
+        public static final int PRIMARY = 0; // untested
+        public static final int SECONDARY = 0; // untested
+
+        public static final int FOTT_MOTOR = 0; // untested
+
+        public static final int SWITCHER = 0; // untested
+        public static final int ELEV_BRAKE = 0; // untested
+        public static final int CLIMB_BRAKE = 0; // untested
+
+        public static final int ELEV_TOP_LIM = 0; // untested
+        public static final int ELEV_BOT_LIM = 0; // untested
+        public static final int CLIMB_LIM_SWITCH= 0; // untested
     }
 
 }
